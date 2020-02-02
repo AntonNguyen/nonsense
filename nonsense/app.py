8364d88be1ec35dfd8eaac42344ae9820691f5c4
AntonNguyen <afnguyen85@gmail.com>
2020-02-02 09:45:53 -0500

    Moved most of the work into the celery worker

--- nonsense/app.py
+++ nonsense/app.py
@@ -1,69 +1,45 @@
-from datetime import datetime
 from flask import Flask, request, jsonify
 from math import fabs
 from nonsense import task
 from pytz import timezone
 from random import choice
 
-
 import hashlib
 import hmac
 import os
 import re
-import redis
 import time
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
-redisClient = redis.from_url(os.getenv('REDIS_URL'), db=1)
 
 
 @app.route('/nonsense', methods=['POST'])
 def nonsense_response():
     if not verify_slack_request():
+        print("Unauthorized access attempt")
         return jsonify({}), 403
 
     data = request.form
     text = data.get('text', '').lower().strip()
     team_id = data.get('team_id')
     channel_id = data.get('channel_id')
-    print(f"Request received from team '{team_id}' in channel '{channel_id}'")
+    user_id = data.get('user_id')
+    print(f"Request received from team '{team_id}' in channel '{channel_id}' from user '{user_id}")
+    task.handle_request.delay(team_id, channel_id, user_id, text)
 
-    last_infraction = get_current_record(team_id)
-
-    if text == "status":
-        tz = timezone('EST')
-        today = datetime.now(tz)
-        delta = today - last_infraction
-        task.upload_image.delay(channel_id, delta.days)
-        return generate_response(status_message(delta.days))
-
-    if text == "report infraction":
-        report_infraction(team_id, last_infraction)
-        return generate_response(report_message())
-
-    report_infraction(team_id, last_infraction)
-    return generate_response(unknown_command_message(text))
-
-
-def get_current_record(team_id):
-    if not redisClient.exists(team_id):
-        tz = timezone('EST')
-        today = datetime.now(tz)
-
-        redisClient.set(team_id, today.isoformat())
-        return today
-    current_record = redisClient.get(team_id)
-    return datetime.fromisoformat(current_record.decode('utf-8'))
-
-
-def report_infraction(team_id, last_infraction):
-    tz = timezone('EST')
-    today = datetime.now(tz)
-    if today.date() == last_infraction.date():
-        return
-
-    redisClient.set(team_id, today.isoformat())
+    return jsonify({
+    "response_type": "in_channel",
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": acknowledgement_message()
+            }
+        }
+    ]
+})
 
 
 def verify_slack_request():
@@ -86,60 +62,20 @@ def verify_slack_request():
     return hmac.compare_digest(my_signature, slack_signature)
 
 
-def status_message(days):
+def acknowledgement_message():
     messages = [
-        "This office has been nonsense-free for {days} days",
-        "The office is on a nonsense-free streak of {days} days!",
-        "Only {days} days. This is ridiculous",
-        "Fun fact: Ecobee is on a nonsense-free streak of 368 days. This office is only on {days} days",
-        "{days} days. Let’s not get crazy and ruin our no-nonsense streak, all right? So, for instance, if you’re expecting a fax today, please don’t yell out, \"Michael J. Fax from Fax to the Future.\" Ok? That’s nonsense.",
+        "Affirmative",
+        "OK boomer",
+        "Roger, roger",
+        "Okay.",
+        "Okee day! Disa happenings.",
+        "ACK",
+        "Roger that.",
+        "As you wish.",
+        "Right on!",
+        "Uh-huh",
+        "Gladly",
+        "Absolutely I do",
     ]
 
-    return choice(messages).format(days=days)
-
-
-def unknown_command_message(command):
-    messages = [
-        "'{command}' is not in the supported-commands file. This incident will be reported.",
-        "Exception in thread \"main\" java.lang.NoClassDefFoundError: {command}",
-        "NameError: name '{command}' is not defined",
-        "errors.New(\"{command} not found\")",
-        "Ahhhhhh! This command doesn't exist!",
-        "Some of the co-ops forgot to handle the '{command}' command. Thanks Anurag.",
-        "I couldn't find the '{command}' command. Here's a pleasant song to listen to while I search: https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        "Your '{command}' command ain't here, kid.",
-        "This is not the command you're looking for.",
-        "Yeaaah what's happening? We went ahead and stopped supporting the '{command}' command, so if you could just go ahead and use the nonsense bot correctly, that would be great.",
-        "Failed to run the '{command}' command. '{command}' is not supported on this bot."
-    ]
-    return choice(messages).format(command=command)
-
-
-def report_message():
-    messages = [
-        "Thank you for keeping tnis a no-nonsense office.",
-        "Good call. Together we run a no-nonsense office.",
-        "That’s obviously nonsense. Nonsense. And what percentage of nonsense do we tolerate in this office? Right. Zero. No nonsense. You can't have nonsense.",
-        "Thank you for stamping out this enormous source of overlooked PFN.",
-        "A+ reporting!",
-        "Well played, old chap!",
-        "GG."
-    ]
     return choice(messages)
-
-
-def generate_response(text):
-    return jsonify({
-        "response_type": "in_channel",
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": text
-                }
-            }
-        ]
-    })
-
-
--- nonsense/task.py
+++ nonsense/task.py
@@ -1,17 +1,53 @@
+from datetime import datetime
 from io import BytesIO
 from nonsense.nonsense import Nonsense, NonenseException
+from random import choice
 
 import celery
 import os
+import redis
 import slack
 
 app = celery.Celery(__name__)
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
+redisClient = redis.from_url(os.getenv('REDIS_URL'), db=1)
+
 SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 
+
 @app.task
+def handle_request(team_id, channel_id, user_id, text):
+    print(f"Task received from team '{team_id}' in channel '{channel_id}' from user '{user_id}")
+    last_infraction = get_current_record(team_id)
+
+    if text == "status":
+        tz = timezone('EST')
+        today = datetime.now(tz)
+        delta = today - last_infraction
+        upload_image(channel_id, delta.days)
+        post_message(channel_id, status_message(delta.days))
+        return
+
+    if text == "report infraction":
+        report_infraction(team_id, last_infraction)
+        post_message(channel_id, report_message())
+        return
+
+    report_infraction(team_id, last_infraction)
+    post_message(channel_id, unknown_command_message(text))
+
+
+def report_infraction(team_id, last_infraction):
+    tz = timezone('EST')
+    today = datetime.now(tz)
+    if today.date() == last_infraction.date():
+        return
+
+    redisClient.set(team_id, today.isoformat())
+
+
 def upload_image(channel_id, days):
     client = slack.WebClient(token=SLACK_TOKEN)
     try:
@@ -28,3 +64,65 @@ def upload_image(channel_id, days):
         print(response)
     except NonenseException as e:
         return generate_response(str(e))
+
+
+def post_message(channel_id, text):
+    client = slack.WebClient(token=SLACK_TOKEN)
+    response = client.chat_postMessage(
+        text=text,
+        channels=channel_id
+    )
+    print(response)
+
+
+def get_current_record(team_id):
+    if not redisClient.exists(team_id):
+        tz = timezone('EST')
+        today = datetime.now(tz)
+
+        redisClient.set(team_id, today.isoformat())
+        return today
+    current_record = redisClient.get(team_id)
+    return datetime.fromisoformat(current_record.decode('utf-8'))
+
+
+def status_message(days):
+    messages = [
+        "This office has been nonsense-free for {days} days",
+        "The office is on a nonsense-free streak of {days} days!",
+        "Only {days} days. This is ridiculous",
+        "Fun fact: Ecobee is on a nonsense-free streak of 368 days. This office is only on {days} days",
+        "{days} days. Let’s not get crazy and ruin our no-nonsense streak, all right? So, for instance, if you’re expecting a fax today, please don’t yell out, \"Michael J. Fax from Fax to the Future.\" Ok? That’s nonsense.",
+    ]
+
+    return choice(messages).format(days=days)
+
+
+def unknown_command_message(command):
+    messages = [
+        "'{command}' is not in the supported-commands file. This incident will be reported.",
+        "Exception in thread \"main\" java.lang.NoClassDefFoundError: {command}",
+        "NameError: name '{command}' is not defined",
+        "errors.New(\"{command} not found\")",
+        "Ahhhhhh! This command doesn't exist!",
+        "Some of the co-ops forgot to handle the '{command}' command. Thanks Anurag.",
+        "I couldn't find the '{command}' command. Here's a pleasant song to listen to while I search: https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "Your '{command}' command ain't here, kid.",
+        "This is not the command you're looking for.",
+        "Yeaaah what's happening? We went ahead and stopped supporting the '{command}' command, so if you could just go ahead and use the nonsense bot correctly, that would be great.",
+        "Failed to run the '{command}' command. '{command}' is not supported on this bot."
+    ]
+    return choice(messages).format(command=command)
+
+
+def report_message():
+    messages = [
+        "Thank you for keeping tnis a no-nonsense office.",
+        "Good call. Together we run a no-nonsense office.",
+        "That’s obviously nonsense. Nonsense. And what percentage of nonsense do we tolerate in this office? Right. Zero. No nonsense. You can't have nonsense.",
+        "Thank you for stamping out this enormous source of overlooked PFN.",
+        "A+ reporting!",
+        "Well played, old chap!",
+        "GG."
+    ]
+    return choice(messages)
